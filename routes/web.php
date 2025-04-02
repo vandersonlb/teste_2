1f77ab3acdf03eb07433c0f863403b1d39fdb81d
Vanderson Luis <vandersonlb@hotmail.com>
2025-04-02 20:50:34 -0300

    testando api

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health', function () {
+    return response()->json(
+        [ 'status' => 'UP', 'timestamp' => now() ],
+        202,
+        [ 'Accept' => 'application/json' ]
+    );
+});
